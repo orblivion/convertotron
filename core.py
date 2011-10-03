dcb9af87917aa00043d3a95d5f5f185b5764b406
Daniel Krol <orblivion@gmail.com>
2011-10-02 20:52:54 -0500

    Pulls out a "get best conversion_method" function

--- core.py
+++ core.py
@@ -4,7 +4,10 @@ import subprocess
 def identify(fname):
     return formats.FLAC
 
-def interaction(fname, output_fname, output_formats = None, output_location = None, settings = None):
+def find_best_method(output_options):
+    return output_options[0]
+
+def convert(fname, output_fname, output_formats = None, output_location = None, settings = None):
 
 #    if output_location:       # "I want to play this on my iPod!" (cue from Miro)
 #        output_formats = get_format(output_location, settings) # aac, etc
@@ -21,10 +24,13 @@ def interaction(fname, output_fname, output_formats = None, output_location = No
 #        output_options.append(get_outputs(input_format, of, options))
 #
 #    # considerations for what's installed, what the user cares to install
+
+
+    conversion_methods = formats.mapping[(input_format, formats.WAV)]
 #    map (check_installed,   output_options)
-#    best = find_best (output_options)
 
+    best_method = find_best_method (conversion_methods)
+    arguments = [arg % {"in_file": fname, "out_file": output_fname } for arg in best_method["arguments"] ]
 
-    conversion_method = formats.mapping[(input_format, formats.WAV)][0]
-    arguments = [arg % {"in_file": fname, "out_file": output_fname } for arg in conversion_method["arguments"] ]
-    subprocess.call([conversion_method['program']] + arguments) 
+    # Run it
+    subprocess.call([best_method['program']] + arguments) 
